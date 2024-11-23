761cf12ed6ac8aa8fc5a6027782ca23a36a30ab7
Kayan Reid <kreid@Zipcoders-MBP.fios-router.home>
2024-11-23 11:27:00 -0500

    Added AnmalTest class as a Test Class to the test directory.

---
+++ src/test/java/com/zipcodewilmington/froilansfarm/AnimalTest.java
@@ -0,0 +1,8 @@
+package com.zipcodewilmington.froilansfarm;
+
+public class AnimalTest {
+    //Animal is a NoiseMaker and Eater
+    //Horse is an Animal and Rideable
+    //Chicken is an Animal
+    // and a Produce which yield an EdibleEgg if hasBeenFertilized flag is false.
+}
