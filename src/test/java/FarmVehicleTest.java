92208f3d9b5a72854eb5a0ac05b1cbfe93989218
sgreloch <samuelgreloch@yahoo.com>
2024-11-24 17:15:59 -0500

    test

--- src/test/java/FarmVehicleTest.java
+++ src/test/java/FarmVehicleTest.java
@@ -1,8 +1,66 @@
+import com.zipcodewilmington.froilansfarm.Trator;
+import org.junit.jupiter.api.Test;
+import org.testng.Assert;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class FarmVehicleTest {
+public class FarmVehicleTest {
+
+    @Test
+
+    public void VehicleMakesNoiseTest (){
 
 
+        Assert.assertTrue(Trator.vehcileMakesNoise);
+    }
 
+
+    @Test
+
+    public void VehicleIsRidableTest(){
+
+    }
+
+    @Test
+
+    public void CanOperateTest(){
+
+    }
+
+    @Test
+
+    public void isVehicleTest(){
+
+    }
+
+    @Test
+
+    public void CanFly(){
+
+    }
+
+    @Test
+
+    public void isAircraftTest(){
+
+    }
+
+    @Test
+
+    public void FertilizeTest(){
+
+    }
+
+    @Test
+
+    public void HarvestTest(){
+
+    }
+
+    @Test
+
+    public void CanOperate(){
+
+    }
 
 }
