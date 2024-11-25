62e2a9cea3db790cfbc6c355086e9d6fdef65c5f
rjohnson0851 <ron21johnson@comcast.net>
2024-11-25 09:10:02 -0500

    Tests

---
+++ src/test/java/Days/Monday.java
@@ -0,0 +1,17 @@
+package Days;
+
+import org.junit.jupiter.api.Test;
+
+public class Monday {
+
+    @Test
+    public void MorningRoutine(){
+
+    }
+
+
+    @Test
+    public void FertilizingTest(){
+
+    }
+}
