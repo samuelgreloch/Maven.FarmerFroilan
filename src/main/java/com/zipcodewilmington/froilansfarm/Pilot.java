7aab12e286ee44e0e50c156ff713e70b1a664fb8
sgreloch <samuelgreloch@yahoo.com>
2024-11-25 08:58:31 -0500

    test

---
+++ src/main/java/com/zipcodewilmington/froilansfarm/Pilot.java
@@ -0,0 +1,6 @@
+package com.zipcodewilmington.froilansfarm;
+
+//public interface Pilot implements Rider<A> {
+
+
+//}
---
+++ src/test/java/com/zipcodewilmington/froilansfarm/PilotTest.java
@@ -0,0 +1,7 @@
+package com.zipcodewilmington.froilansfarm;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PilotTest {
+
+}
