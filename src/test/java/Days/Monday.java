e64e0fb60361979f107a0b1e678381065cfd32e7
rjohnson0851 <ron21johnson@comcast.net>
2024-11-25 14:15:59 -0500

    more done

--- src/test/java/Days/Monday.java
+++ src/test/java/Days/Monday.java
@@ -18,17 +18,6 @@ public class Monday {
 
     @Test
     public void FertilizingTest(){
-        Person person = new Person("Froilanda");
-        CropDuster cropDuster = new CropDuster();
 
-        Crop cropRow1 = new Crop();
-        Crop cropRow2 = new Crop();
-        Crop cropRow3 = new Crop();
-
-        assertFalse(cropRow1.hasBeenFertilized());
-        assertFalse(cropRow2.hasBeenFertilized());
-        assertFalse(cropRow3.hasBeenFertilized());
-
-       // cropDuster.ferilize(person, cropRow1);
     }
 }
