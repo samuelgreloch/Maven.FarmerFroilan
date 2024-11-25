bdd0e264f4eb62bd1e5968307dde8e3711cd8033
Alyssa Resh <aresh@Zipcoders-MacBook-Pro.local>
2024-11-25 14:26:57 -0500

    tuesday test?

--- src/test/java/Days/Tuesday.java
+++ src/test/java/Days/Tuesday.java
@@ -3,6 +3,7 @@ package Days;
 import com.zipcodewilmington.froilansfarm.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.testng.AssertJUnit;
 
 import static org.testng.AssertJUnit.assertFalse;
 import static org.testng.AssertJUnit.assertTrue;
@@ -16,14 +17,46 @@ public class Tuesday {
     }
 
     @Test
-    public void HarvestTest() {
+    public void NotHarvestedTest() {
     Person person = new Person("Froilan");
-    CropDuster cropDuster = new CropDuster("");
+    Tractor tractor = new Tractor(false, true, false);
+
+    CropRow<CornStalk> cornRow = new CropRow<>();
+    CropRow<TomatoPlant> tomatoRow = new CropRow<>();
+    CornStalk corn = new CornStalk();
+    TomatoPlant tomato = new TomatoPlant();
+    cornRow.plant(corn);
+    tomatoRow.plant(tomato);
+
+    cornRow.stream().map(Crop::hasBeenHarvested).forEach(AssertJUnit::assertFalse);
+    tomatoRow.stream().map(Crop::hasBeenHarvested).forEach(AssertJUnit::assertFalse);
+    }
+
+    @Test
+    public void HarvestedTest() {
+        Person person = new Person("Froilan");
+        Tractor tractor = new Tractor(false, true, false);
+
+        CropRow<CornStalk> cornRow = new CropRow<>();
+        CropRow<TomatoPlant> tomatoRow = new CropRow<>();
+        CornStalk corn = new CornStalk();
+        TomatoPlant tomato = new TomatoPlant();
+        cornRow.plant(corn);
+        tomatoRow.plant(tomato);
+
+        if (tractor.canHarvest()) {
+            for (CornStalk crop : cornRow) {
+                crop.harvest();
+            }
+        }
+
+        if (tractor.canHarvest()) {
+            for (TomatoPlant crop : tomatoRow) {
+                crop.harvest();
+            }
+        }
+
+        cornRow.stream().map(Crop::hasBeenHarvested).forEach(AssertJUnit::assertTrue);
+        tomatoRow.stream().map(Crop::hasBeenHarvested).forEach(AssertJUnit::assertTrue);
     }
 }
-//* On `Tuesday`, `Froilan` uses his `Tractor` to `harvest` each `Crop` in each `CropRow`.
-//check & make sure:
-//Crops are harvested: After the harvest() method is called on the Tractor, all Crop objects in each CropRow should have their hasBeenHarvested flag set to true.
-//Crops yield the correct edible objects after being harvested (if applicable).
-//Froilan's actions (riding, feeding, and eating) don't interfere with the harvesting process but are part of the test setup.
-//Test the Harvesting Process: Ensure that after calling harvest(), the hasBeenHarvested flag of all crops is set to true.
