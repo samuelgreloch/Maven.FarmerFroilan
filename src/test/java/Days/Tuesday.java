b3dba088b9fa67d3d6db4f8ca5e788ecb7abf9c5
Alyssa Resh <aresh@Zipcoders-MacBook-Pro.local>
2024-11-25 11:56:56 -0500

    tuesday tests - saving before pull

--- src/test/java/Days/Tuesday.java
+++ src/test/java/Days/Tuesday.java
@@ -1,8 +1,16 @@
 package Days;
 
+import com.zipcodewilmington.froilansfarm.*;
 import org.junit.jupiter.api.Test;
 
+import static org.testng.AssertJUnit.assertFalse;
+import static org.testng.AssertJUnit.assertTrue;
+
 public class Tuesday {
+    FarmField farmField;
+    Trator trator;
+    Person<?> Froilan;
+
 
     @Test
     public void MorningRoutine(){
@@ -11,6 +19,26 @@ public class Tuesday {
 
     @Test
     public void HarvestingTest(){
+        trator.harvest(farmField);
 
+        for (CropRow<?> row : farmField) {
+                row.showCrops();
+            }
     }
+//    public void testHarvestingOnTuesday() {
+//        // Froilan uses the Tractor to harvest the crops on Tuesday
+//        froilan.useTractorToHarvest(tractor, farm.getField());
+//
+//        // After harvesting, check that all crops in each row have been harvested
+//        for (CropRow row : farm.getField().getCropRows()) {
+//            for (Crop crop : row.getCrops()) {
+//                assertTrue("Crop should be harvested", crop.hasBeenHarvested());
+//            }
+//        }
 }
+//* On `Tuesday`, `Froilan` uses his `Tractor` to `harvest` each `Crop` in each `CropRow`.
+//check & make sure:
+//Crops are harvested: After the harvest() method is called on the Tractor, all Crop objects in each CropRow should have their hasBeenHarvested flag set to true.
+//Crops yield the correct edible objects after being harvested (if applicable).
+//Froilan's actions (riding, feeding, and eating) don't interfere with the harvesting process but are part of the test setup.
+//Test the Harvesting Process: Ensure that after calling harvest(), the hasBeenHarvested flag of all crops is set to true.
