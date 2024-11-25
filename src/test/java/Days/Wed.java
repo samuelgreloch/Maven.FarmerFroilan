0cff383bed9bdc978cd137caeeaf45d67e417d10
DeeptiMishra@1512 <mishra.deeptimishra@gmail.com>
2024-11-25 12:03:07 -0500

    commit

--- src/test/java/Days/Wed.java
+++ src/test/java/Days/Wed.java
@@ -1,12 +1,44 @@
 package Days;
 
+import com.zipcodewilmington.froilansfarm.EarCorn;
+import com.zipcodewilmington.froilansfarm.Horse;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class Wed {
 
+//Plant Corn on cropRow 4
     @Test
-    public void MorningRoutine(){
+    public void plantCornTest(){
+
+
+
+
 
     }
+
+// Feed each horse 6 EarCorn
+    @Test
+    public void feedEachHorseTest(){
+
+            EarCorn corn = new EarCorn();
+            //  List<Horse> horseList = new ArrayList<>();
+
+            for (int i = 1; i <= 10; i++) {
+                Horse horse = new Horse(i);
+                //Feeding 3 corns to each Horse
+                horse.eat(corn);
+                horse.eat(corn);
+                horse.eat(corn);
+                horse.eat(corn);
+                horse.eat(corn);
+                horse.eat(corn);
+
+                Assertions.assertEquals(6, horse.numberComsumed(corn));
+
+            }
+
+        }
+
 }
 
