d86db4cb4843f35220ea43f792a2379bbf09322d
DeeptiMishra@1512 <mishra.deeptimishra@gmail.com>
2024-11-25 00:14:08 -0500

    Completed test cases for morning routine

--- src/test/java/com/zipcodewilmington/froilansfarm/EveryMorningRoutineTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/EveryMorningRoutineTest.java
@@ -36,7 +36,7 @@ public class EveryMorningRoutineTest {
   @Test
     public void feedingEachHorse() {
       EarCorn corn = new EarCorn();
-      List<Horse> horseList = new ArrayList<>();
+    //  List<Horse> horseList = new ArrayList<>();
 
       for (int i = 1; i <= 10; i++) {
           Horse horse = new Horse(i);
@@ -46,77 +46,120 @@ public class EveryMorningRoutineTest {
           horse.eat(corn);
 
           Assertions.assertEquals(3, horse.numberComsumed(corn));
-          System.out.println(horse.numberComsumed(corn));
 
       }
 
   }
 
 
+  //*******Froilan Breakfast Scenes*********//
+  //Eating 2 Tomato test
 @Test
-    public void froilanBreakFastScenesTest(){
+    public void froilanBreakFastScenesTest1(){
     Person froilan = new Person("Froilan");
-    Person person2 = new Person("Froilanda");
-
 
     Tomato tomato = new Tomato();
-    EarCorn corn = new EarCorn();
-    Egg egg = new Egg();
-
 
     // 2 tomato
     froilan.eat(tomato);
     froilan.eat(tomato);
 
-    //one corn
-    froilan.eat(corn);
-
-    //6 eggs
-    froilan.eat(egg);
-    froilan.eat(egg);
-    froilan.eat(egg);
-    froilan.eat(egg);
-    froilan.eat(egg);
-    froilan.eat(egg);
-
     Assertions.assertTrue(froilan.hasEaten(tomato));
-    Assertions.assertTrue(froilan.hasEaten(corn));
-    Assertions.assertTrue(froilan.hasEaten(egg));
     Assertions.assertEquals(2,froilan.numberComsumed(tomato));
-    Assertions.assertEquals(1,froilan.numberComsumed(corn));
-    Assertions.assertEquals(6,froilan.numberComsumed(egg));
 
 }
 
+//Eating 1 EarCorn Test
     @Test
-    public void froilandaBreakFastScenesTest(){
+    public void froilanBreakFastScenesTest2(){
+        Person froilan = new Person("Froilan");
+
+        EarCorn corn = new EarCorn();
+
+        //one corn
+        froilan.eat(corn);
+
+
+        Assertions.assertTrue(froilan.hasEaten(corn));
+        Assertions.assertEquals(1,froilan.numberComsumed(corn));
+
+
+    }
+
+
+//Eating 6 Eggs Test
+    @Test
+    public void froilanBreakFastScenesTest3(){
+        Person froilan = new Person("Froilan");
+
+        Egg egg = new Egg();
+
+        //6 eggs
+        froilan.eat(egg);
+        froilan.eat(egg);
+        froilan.eat(egg);
+        froilan.eat(egg);
+        froilan.eat(egg);
+        froilan.eat(egg);
+
+        Assertions.assertTrue(froilan.hasEaten(egg));
+        Assertions.assertEquals(6,froilan.numberComsumed(egg));
+
+    }
+
+
+//*******Froilanda Breakfast Scenes*********//
+
+//Froilanda Eating Tomato test
+    @Test
+    public void froilandaBreakFastScenesTest1(){
 
         Person froilanda = new Person("Froilanda");
 
-
         Tomato tomato = new Tomato();
-        EarCorn corn = new EarCorn();
-        Egg egg = new Egg();
-
 
         // 1 tomato
         froilanda.eat(tomato);
+
+        Assertions.assertTrue(froilanda.hasEaten(tomato));
+
+        Assertions.assertEquals(1,froilanda.numberComsumed(tomato));
+
+    }
+
+ //Froilanda Eating EarCorn Test
+    @Test
+    public void froilandaBreakFastScenesTest2(){
+
+        Person froilanda = new Person("Froilanda");
+
+        EarCorn corn = new EarCorn();
 
         //2 corn
         froilanda.eat(corn);
         froilanda.eat(corn);
 
+        Assertions.assertTrue(froilanda.hasEaten(corn));
+
+        Assertions.assertEquals(2,froilanda.numberComsumed(corn));
+
+    }
+
+
+//Froilanda Eating Egg Test
+    @Test
+    public void froilandaBreakFastScenesTest3(){
+
+        Person froilanda = new Person("Froilanda");
+
+        Egg egg = new Egg();
+
         //2 eggs
         froilanda.eat(egg);
         froilanda.eat(egg);
 
-
-        Assertions.assertTrue(froilanda.hasEaten(tomato));
-        Assertions.assertTrue(froilanda.hasEaten(corn));
         Assertions.assertTrue(froilanda.hasEaten(egg));
 
-        Assertions.assertEquals(1,froilanda.numberComsumed(tomato));
-        Assertions.assertEquals(2,froilanda.numberComsumed(corn));
         Assertions.assertEquals(2,froilanda.numberComsumed(egg));
 
     }
